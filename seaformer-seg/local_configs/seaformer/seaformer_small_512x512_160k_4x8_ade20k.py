701308ea8eb8e67ac9e5b1664f9e955cc6d24e88
Faatemehgheybi4457 <faatila.gheybi1377@gmail.com>
2025-09-27 21:57:18 +0330

    Update seaformer_small_512x512_160k_4x8_ade20k.py

--- seaformer-seg/local_configs/seaformer/seaformer_small_512x512_160k_4x8_ade20k.py
+++ seaformer-seg/local_configs/seaformer/seaformer_small_512x512_160k_4x8_ade20k.py
@@ -17,6 +17,6 @@ lr_config = dict(_delete_=True, policy='poly',
 
 # By default, models are trained on 8 GPUs with 2 images per GPU
 data=dict(samples_per_gpu=4)
-checkpoint_config = dict(by_epoch=False, interval=4000)
+checkpoint_config = dict(by_epoch=False, interval=4000, filename_tmpl='last-small.pth')
 evaluation = dict(interval=4000, metric='mIoU', pre_eval=True)
 find_unused_parameters=True
