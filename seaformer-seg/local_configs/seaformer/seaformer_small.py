619f366d9c9b0908a755eaebb5f019d5e8cfd9d5
Faatemehgheybi4457 <faatila.gheybi1377@gmail.com>
2025-09-28 22:42:34 +0330

    Update seaformer_small.py

--- seaformer-seg/local_configs/seaformer/seaformer_small.py
+++ seaformer-seg/local_configs/seaformer/seaformer_small.py
@@ -37,7 +37,8 @@ model = dict(
         num_heads=model_cfgs['num_heads'],
         drop_path_rate=model_cfgs['drop_path_rate'],
         norm_cfg=norm_cfg,
-        init_cfg=dict(type='Pretrained', checkpoint='/teamspace/studios/this_studio/SeaFormer_S_bs32_39.5.pth')
+        init_cfg=dict(type='Pretrained')
+        #init_cfg=dict(type='Pretrained', checkpoint='/teamspace/studios/this_studio/SeaFormer_S_bs32_39.5.pth')
     ),
     decode_head=dict(
         type='LightHead',
